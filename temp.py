00743edb3ff600f239a956e85dcbb0a5932a481b
anupkafle <anupkafle24@gmail.com>
2022-10-15 22:42:01 +0545

    Temp converter

---
+++ temp.py
@@ -0,0 +1,10 @@
+celsius_1 = float(input("Temperature value in degree Celsius: " ))  
+Fahrenheit_1 = (celsius_1 * 1.8) + 32  
+print('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_1, Fahrenheit_1))  
+  
+print("----OR----")  
+celsius_2 = float (input("Temperature value in degree Celsius: " ))  
+Fahrenheit_2 = (celsius_2 * 9/5) + 32  
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_2, Fahrenheit_2))  
